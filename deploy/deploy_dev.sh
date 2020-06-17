e4e44f5f1c3904d6748e0945410b2e1150ee4740
Heisenberg <arturo.lucas@pucp.edu.pe>
2020-06-17 12:58:16 -0500

    feature: adding continuous integration fourth strike

--- deploy/deploy_dev.sh
+++ deploy/deploy_dev.sh
@@ -3,5 +3,7 @@ set -e
 eval $(ssh-agent -s)
 echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
+./deploy/disable_host_key_checking.sh
+
 echo "DEPLOYING DEVELOP BRANCH INTO ${SERVER}"
 ssh ubuntu@"${SERVER}" 'bash -s' < ./deploy/run_dev.sh
