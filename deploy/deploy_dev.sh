4efe2305e268982074e64083454d7c97cdeee9b0
Heisenberg <arturo.lucas@pucp.edu.pe>
2020-06-17 13:06:24 -0500

    feature: continuous integration

--- deploy/deploy_dev.sh
+++ deploy/deploy_dev.sh
@@ -3,6 +3,7 @@ set -e
 eval $(ssh-agent -s)
 echo "$PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
+chmod a+x ./deploy/disable_host_key_checking.sh
 ./deploy/disable_host_key_checking.sh
 
 echo "DEPLOYING DEVELOP BRANCH INTO ${SERVER}"
